e7dda293f3feba35e4df212c5ff854b1f8f74b09
aitoAarni <aarni.rechardt@gmail.com>
2026-03-20 17:48:56 +0200

    week2 done

---
+++ rechardt_aarni_ex2/p4/b.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <format>
+#include <time.h>
+
+constexpr int N = 1'000;
+long a[N][N];
+long b[N][N];
+long c[N][N];
+long t[N][N];
+
+void fill_matrix(long matrix[N][N]) {
+    long seed {43248};
+    for (int i {0}; i < N; i++) {
+        for (int j {0}; j < N; j++) {
+            seed = (seed * 3 + 2) % 6103828;
+            matrix[i][j] = seed;
+        }
+    }
+}
+
+void print_matrix(long matrix[N][N]) {
+    for (int i {0}; i < N; i++) {
+        for (int j {0}; j < N; j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+void transpose_matrix(long m[N][N]) {
+    for (int i {0}; i < N; i++) {
+        for (int j {i+1}; j < N; j++) {
+            std::swap(m[i][j], m[j][i]);
+        }
+    }
+}
+
+double matrix_multiplication(long a[N][N], long b[N][N], long c[N][N]) {
+    // I have used a better memory access pattern here than the default
+    // B has to be transpoed before so it is same as C=A*B
+    auto t1 = clock();
+    for (int row {0}; row < N; row++) {
+        for (int column {0}; column < N; column++) {
+            for (int i {0}; i < N; i++) {
+                c[row][column] += a[row][i] * b[column][i];
+            }
+        }
+    }
+    auto t2 = clock();
+    auto cputime = (double)(t2-t1)/CLOCKS_PER_SEC;
+    return cputime;
+}
+
+
+
+int main() {
+    fill_matrix(a);
+    fill_matrix(b);
+    transpose_matrix(a);
+    transpose_matrix(b);
+    auto time = matrix_multiplication(a, b, c);
+    std::cout << "Matrix multiplication A^T*B=C took: " << time << " seconds. (" << c[4][3] << ")\n";
+    return 0;
+}
---
+++ rechardt_aarni_ex2/p4/bad_mem_access_b.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <format>
+#include <time.h>
+
+constexpr int N = 1'000;
+long a[N][N];
+long b[N][N];
+long c[N][N];
+long t[N][N];
+
+void fill_matrix(long matrix[N][N]) {
+    long seed {43248};
+    for (int i {0}; i < N; i++) {
+        for (int j {0}; j < N; j++) {
+            seed = (seed * 3 + 2) % 6103828;
+            matrix[i][j] = seed;
+        }
+    }
+}
+
+void print_matrix(long matrix[N][N]) {
+    for (int i {0}; i < N; i++) {
+        for (int j {0}; j < N; j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+void transpose_matrix(long m[N][N]) {
+    for (int i {0}; i < N; i++) {
+        for (int j {i+1}; j < N; j++) {
+            std::swap(m[i][j], m[j][i]);
+        }
+    }
+}
+
+double matrix_multiplication_a(long a[N][N], long b[N][N], long c[N][N]) {
+    // I have used a better memory access pattern here than the default
+    // B has to be transpoed before so it is same as C=A*B
+    auto t1 = clock();
+    for (int row {0}; row < N; row++) {
+        for (int column {0}; column < N; column++) {
+            for (int i {0}; i < N; i++) {
+                c[row][column] += a[i][row] * b[i][column];
+            }
+        }
+    }
+    auto t2 = clock();
+    auto cputime = (double)(t2-t1)/CLOCKS_PER_SEC;
+    return cputime;
+}
+
+
+
+int main() {
+    fill_matrix(a);
+    fill_matrix(b);
+    auto time = matrix_multiplication_a(a, b, c);
+    std::cout << "Matrix multiplication (awful mem access) A^T*B=C took: " << time << " seconds. (" << c[4][3] << ")\n";
+    return 0;
+}
