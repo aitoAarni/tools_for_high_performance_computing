0294b015ae232b63c31bf37b0b90801be4a21c6b
aitoAarni <aarni.rechardt@gmail.com>
2026-03-26 20:27:53 +0200

    p2 before chaging file

--- rechardt_aarni_ex3/p2/ex3p2.c
+++ rechardt_aarni_ex3/p2/ex3p2.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 #define N 15000
 int a[N][N];
@@ -11,21 +12,17 @@ void main() {
 
 
   /* Begin measurement */
+  t1 = clock();
   for (i=0;i<N;i++) 
     for (j=0;j<N;j++) 
       a[i][j]=(i+j)/2;
+  t2 = clock();
   /* End measurement */
-
-
+  
+  
   for (i=0;i<N;i+=NPRINT)
-    for (j=0;j<N;j+=NPRINT)
-      printf("%d ",a[i][j]);
+  for (j=0;j<N;j+=NPRINT)
+  printf("%d ",a[i][j]);
   printf("\n");
-
-
+  printf("Time: %f seconds\n",(t2-t1)/(double)CLOCKS_PER_SEC);
 }
-
-
-  
-  
-    
